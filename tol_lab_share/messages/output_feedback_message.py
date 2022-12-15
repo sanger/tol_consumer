ef520f0f1e6a3eeab32103f3970cc0f70304ace0
Eduardo Martin Rojo <emr@sanger.ac.uk>
2022-12-15 15:12:03 +0000

    For mypy getter and setters must be near in code

--- tol_lab_share/messages/output_feedback_message.py
+++ tol_lab_share/messages/output_feedback_message.py
@@ -3,27 +3,43 @@ from typing import List, Optional
 
 class OutputFeedbackMessage:
     def __init__(self):
-        self._source_message_uuid : Optional[str] = None
-        self._count_of_total_samples : Optional[int] = None
-        self._count_of_valid_samples = None
-        self._operation_was_error_free = None
-        self._errors = []
+        self._source_message_uuid: Optional[bytes] = None
+        self._count_of_total_samples: Optional[int] = None
+        self._count_of_valid_samples: Optional[int] = None
+        self._operation_was_error_free: Optional[bool] = None
+        self._errors: List[List[str]] = []
 
     @property
-    def source_message_uuid(self) -> str:
+    def source_message_uuid(self) -> Optional[bytes]:
         return self._source_message_uuid
 
+    @source_message_uuid.setter
+    def source_message_uuid(self, value: bytes) -> None:
+        self._source_message_uuid = value
+
     @property
-    def count_of_total_samples(self) -> int:
+    def count_of_total_samples(self) -> Optional[int]:
         return self._count_of_total_samples
 
-    @property
-    def count_of_valid_samples(self) -> int:
-        return self._count_of_valid_samples
+    @count_of_total_samples.setter
+    def count_of_total_samples(self, value: int) -> None:
+        self._count_of_total_samples = value
 
     @property
-    def operation_was_error_free(self) -> int:
+    def count_of_valid_samples(self) -> Optional[int]:
+        return self._count_of_valid_samples
+
+    @count_of_valid_samples.setter
+    def count_of_valid_samples(self, value: int) -> None:
+        self._count_of_valid_samples = value
+
+    @property
+    def operation_was_error_free(self) -> Optional[bool]:
         return self._operation_was_error_free
+
+    @operation_was_error_free.setter
+    def operation_was_error_free(self, value: bool) -> None:
+        self._operation_was_error_free = value
 
     @property
     def errors(self) -> List[List[str]]:
@@ -31,19 +47,3 @@ class OutputFeedbackMessage:
 
     def add_error(self, type_id, origin, sample_uuid, field, description):
         self._errors.append([type_id, origin, sample_uuid, field, description])
-
-    @source_message_uuid.setter
-    def source_message_uuid(self, value: str) -> None:
-        self._source_message_uuid = value
-
-    @count_of_total_samples.setter
-    def count_of_total_samples(self, value: int) -> None:
-        self._count_of_total_samples = value
-
-    @count_of_valid_samples.setter
-    def count_of_valid_samples(self, value: int) -> None:
-        self._count_of_valid_samples = value
-
-    @operation_was_error_free.setter
-    def operation_was_error_free(self, value: int) -> None:
-        self._operation_was_error_free = value
