1c638692ec9aa4c87685429a152e9c31548ad0de
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-01-10 16:37:05 +0000

    Added test for DictInput

--- tests/message_properties/test_dict_input.py
+++ tests/message_properties/test_dict_input.py
@@ -5,6 +5,19 @@ def test_dict_input_can_validate_when_valid():
     input = DictInput({"name": 1}, "name")
     assert input.validate()
 
+    input = DictInput(DictInput({"name": {"first": "James"}}, "name"), "first")
+    assert input.validate()
+
+
+def test_dict_input_can_get_value_when_valid():
+    input = DictInput({"name": 1}, "name")
+    assert input.validate()
+
+
+def test_dict_input_can_get_chained_value_when_valid():
+    input = DictInput(DictInput({"name": {"first": "James"}}, "name"), "first")
+    assert input.value == "James"
+
 
 def test_dict_input_can_validate_when_invalid():
     input = DictInput({}, "name")
@@ -17,4 +30,7 @@ def test_dict_input_can_validate_when_invalid():
     assert not input.validate()
 
     input = DictInput({"other": 1}, "name")
+    assert not input.validate()
+
+    input = DictInput(DictInput({"name": {"first": "James"}}, "name"), "second")
     assert not input.validate()
