2cab13fb73c830fa3bb7c5c0060153773d75ceee
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-01-10 16:18:34 +0000

    More tests for input

---
+++ tests/message_properties/test_input.py
@@ -0,0 +1,17 @@
+from tol_lab_share.message_properties.input import Input
+
+
+def test_input_can_validate_everything_true():
+    assert Input(None).validate()
+    assert Input("").validate()
+    assert Input([]).validate()
+    assert Input("test").validate()
+    assert Input(1).validate()
+
+
+def test_input_can_return_value():
+    assert Input(None).value is None
+    assert Input("").value == ""
+    assert Input([]).value == []
+    assert Input("test").value == "test"
+    assert Input(1).value == 1
