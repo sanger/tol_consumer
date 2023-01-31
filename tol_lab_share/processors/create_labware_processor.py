bc0960db3580130cc4eb2f36de9bd48fa78205d4
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-01-31 15:29:27 +0000

    More comments

--- tol_lab_share/processors/create_labware_processor.py
+++ tol_lab_share/processors/create_labware_processor.py
@@ -5,14 +5,26 @@ from lab_share_lib.processing.rabbit_message import RabbitMessage
 from tol_lab_share.messages.output_feedback_message import OutputFeedbackMessage
 from tol_lab_share.messages.input_create_labware_message import InputCreateLabwareMessage
 from tol_lab_share.messages.output_traction_message import OutputTractionMessage
+from typing import Any
 
 from tol_lab_share import error_codes
+from lab_share_lib.rabbit.basic_publisher import BasicPublisher
+from lab_share_lib.rabbit.schema_registry import SchemaRegistry
+
 
 logger = logging.getLogger(__name__)
 
 
 class CreateLabwareProcessor:
-    def __init__(self, schema_registry, basic_publisher, config):
+    """Class to handle consuming create-labware messages from TOL"""
+
+    def __init__(self, schema_registry: SchemaRegistry, basic_publisher: BasicPublisher, config: Any):
+        """Resets data for the processor
+        Parameters:
+        schema_registry (SchemaRegistry) the redpanda schema registry we will us to validate messages
+        basic_publisher (BasicPublisher) instance that will provide the communication with the queues system
+        config (Any) mainconfiguration from the app
+        """
         logger.debug("CreateLabwareProcessor::__init__")
 
         self._basic_publisher = basic_publisher
@@ -20,6 +32,22 @@ class CreateLabwareProcessor:
         self._config = config
 
     def process(self, message: RabbitMessage) -> bool:
+        """Receives a message from rabbitmq. Parses the message with InputCreateLabwareMessage and validates
+        that it is correct. If is correct, it will generate a new OutputTractionMessage and send it to Traction.
+        The result of this operation will be aggregated to a feedback message and this message will be published
+        back into the feedback queue.
+        If there is any error on the input message received, it will add all the errors from the parsing into
+        a feedback message and send message back to the feedback queue.
+        If a feedback message could be generated and sent correctly, this method will return True.
+        If there was any major issue and this feedback message could not be generated and sent, the method will
+        return False. This will also trigger publishing the input message into the dead letters queue so a
+        user can have a look at the message to debug what was the problem.
+        Parameters:
+        message (RabbitMessage) message received in the create-labware queue
+        Returns:
+        boolean indicating that the message could be correctly processed (parsed correctly and sent to traction).
+        If not, it will return False, which will trigger sending this message to the dead letters queue.
+        """
         logger.debug("CreateLabwareProcessor::process")
         logger.debug(f"Received: { message.message }")
 
--- tol_lab_share/processors/update_labware_processor.py
+++ tol_lab_share/processors/update_labware_processor.py
@@ -12,13 +12,26 @@ logger = logging.getLogger(__name__)
 
 
 class UpdateLabwareProcessor:
+    """Class to handle consuming update-labware messages from TOL"""
+
     def __init__(self, schema_registry, basic_publisher, config):
+        """Resets data for the processor
+        Parameters:
+        schema_registry (SchemaRegistry) the redpanda schema registry we will us to validate messages
+        basic_publisher (BasicPublisher) instance that will provide the communication with the queues system
+        config (Any) mainconfiguration from the app
+        """
         logger.debug("UpdateLabwareProcessor::__init__")
         self._encoder = AvroEncoderBinary(schema_registry, RABBITMQ_SUBJECT_UPDATE_LABWARE_FEEDBACK)
         self._basic_publisher = basic_publisher
         self._config = config
 
     def process(self, message: RabbitMessage) -> bool:
+        """Consumes a message for updating labware information. At now no relevant action is attached to
+        this.
+        Returns:
+        bool indicating if the message was correctly consumed
+        """
         logger.debug("UpdateLabwareProcessor::process")
         logger.debug(f"Received: { message.message }")
 
