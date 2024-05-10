1900601115539243a8742f8ccb223610a905ea5d
Stuart McHattie <sm49@sanger.ac.uk>
2024-05-10 16:29:01 +0100

    Improve the standards of unit testing patterns for DictValue

--- tests/messages/properties/simple/test_dict_value.py
+++ tests/messages/properties/simple/test_dict_value.py
@@ -17,29 +17,34 @@ class TestDictValue:
         subject = DictValue(DictValue({"name": {"first": "James"}}, "name"), "first")
         assert subject.value == "James"
 
-    def test_can_validate_when_invalid(self):
+    def test_validates_false_when_dict_is_empty(self):
         subject = DictValue({}, "name")
         assert subject.validate() is False
 
+    def test_validates_false_when_key_missing(self):
         subject = DictValue({"other": 1}, "name")
         assert subject.validate() is False
 
+    def test_validates_false_when_wrapped_dict_key_is_missing(self):
         subject = DictValue(DictValue({"name": {"first": "James"}}, "name"), "second")
         assert subject.validate() is False
 
+    def test_validates_false_when_enclosing_dict_key_is_missing(self):
         subject = DictValue(DictValue({"name": {"first": "James"}}, "WRONG!!!"), "second")
         assert subject.validate() is False
 
-    def test_validates_true_when_optional(self):
+    def test_validates_true_when_optional_but_key_exists(self):
         subject = DictValue({"first_name": "James"}, "first_name", optional=True)
         assert subject.validate() is True
 
-        subject = DictValue({"first_name": "James"}, "last_name", optional=True)
-        assert subject.validate() is True
-
-    def test_can_get_value_when_optional(self):
+    def test_can_get_correct_value_when_optional_but_key_exists(self):
         subject = DictValue({"first_name": "James"}, "first_name", optional=True)
         assert subject.value == "James"
 
+    def test_validates_true_when_optional_and_missing_key(self):
+        subject = DictValue({"first_name": "James"}, "last_name", optional=True)
+        assert subject.validate() is True
+
+    def test_can_get_none_value_when_optional_and_missing_key(self):
         subject = DictValue({"first_name": "James"}, "last_name", optional=True)
         assert subject.value is None
