8cd0e585de535059703ed5934558f439f8881b43
Stuart McHattie <sm49@sanger.ac.uk>
2024-05-21 16:18:23 +0100

    Remove old message handling classes and tests

--- tol_lab_share/messages/properties/complex/__init__.py
+++ tol_lab_share/messages/properties/complex/__init__.py
@@ -1,8 +1,6 @@
 from .date_utc import DateUtc
 from .labware import Labware
-from .library import Library
 from .message_uuid import MessageUuid
-from .request import Request
 from .uuid import Uuid
 
-__all__ = ["DateUtc", "Labware", "Library", "MessageUuid", "Request", "Uuid"]
+__all__ = ["DateUtc", "Labware", "MessageUuid", "Uuid"]
