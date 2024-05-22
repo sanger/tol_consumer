2dbe34a2ca62ed6feff839ac667006fe521b25cf
Stuart McHattie <sm49@sanger.ac.uk>
2024-05-22 15:56:46 +0100

    Supplement the expectations of initialising a record

--- tests/messages/consumed/test_record.py
+++ tests/messages/consumed/test_record.py
@@ -19,29 +19,37 @@ def payload() -> dict[str, Any]:
 
 class TestRecord:
     @pytest.mark.parametrize(
-        "payload, name, parent_path",
+        "name, parent_path, expected_path",
         [
-            ({}, "", ""),
-            ({}, "", "root"),
-            ({}, "name", ""),
-            ({}, "name", "root"),
+            ("", "", ""),
+            ("", "root", "root."),
+            ("name", "", "name"),
+            ("name", "root", "root.name"),
         ],
     )
-    def test_initialises_successfully_with_all_parameters(self, payload, name, parent_path):
+    def test_initialises_successfully_with_all_parameters(self, payload, name, parent_path, expected_path):
         record = Record(payload, name, parent_path)
         assert record is not None
+        assert record._payload == payload
+        assert record._path == expected_path
 
-    def test_initialises_successfully_with_no_name(self):
-        record = Record({}, parent_path="parent_path")
+    def test_initialises_successfully_with_no_name(self, payload):
+        record = Record(payload, parent_path="parent_path")
         assert record is not None
+        assert record._payload == payload
+        assert record._path == "parent_path."
 
-    def test_initialises_successfully_with_no_parent_path(self):
-        record = Record({}, name="name")
+    def test_initialises_successfully_with_no_parent_path(self, payload):
+        record = Record(payload, name="name")
         assert record is not None
+        assert record._payload == payload
+        assert record._path == "name"
 
-    def test_initialises_successfully_with_no_optional_parameters(self):
-        record = Record({})
+    def test_initialises_successfully_with_no_optional_parameters(self, payload):
+        record = Record(payload)
         assert record is not None
+        assert record._payload == payload
+        assert record._path == ""
 
     @pytest.mark.parametrize(
         "name, parent_path, expected_path",
