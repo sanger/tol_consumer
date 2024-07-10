49e2d964c84d7a0d8e7edc80da06a8743f1ff314
Dasun Pubudumal <pubudumald@gmail.com>
2024-07-10 15:37:14 +0100

    create_aliquot_processor.py implementation for process method

--- tol_lab_share/processors/create_aliquot_processor.py
+++ tol_lab_share/processors/create_aliquot_processor.py
@@ -5,7 +5,10 @@ from lab_share_lib.processing.base_processor import BaseProcessor
 from lab_share_lib.processing.rabbit_message import RabbitMessage
 from lab_share_lib.rabbit.basic_publisher import BasicPublisher
 from lab_share_lib.rabbit.schema_registry import SchemaRegistry
+from tol_lab_share.messages.consumed import TractionToWarehouseMessage
+from tol_lab_share.messages.mappers.traction_to_warehouse import TractionToWarehouseMapper
 
+from tol_lab_share.messages.mlwh.create_aliquot_message import CreateAliquotInWarehouseMessage
 
 logger = logging.getLogger(__name__)
 
@@ -33,14 +36,27 @@ class CreateAliquotProcessor(BaseProcessor):
         """Instantiate a CreateAliquotProcessor"""
         return CreateAliquotProcessor(schema_registry, basic_publisher, config)
 
-    def process(self, message: RabbitMessage) -> bool:
+    def process(self, emq_message: RabbitMessage) -> bool:
         """
         1. Receives a message from rabbitmq.
         2. Validates the message with RedPanda schema.
         3. Push messages to the warehouse RMQ.
         """
         logger.debug("CreateAliquotProcessor::process")
+        logger.info(f"Message received: {emq_message}")
 
-        logger.info(f"Message received: {message}")
+        input_message_from_traction = TractionToWarehouseMessage(emq_message.message)
+        output_warehouse_message = CreateAliquotInWarehouseMessage()
 
+        TractionToWarehouseMapper.map(
+            input_message_from_traction,
+            output_warehouse_message
+        )
+        logger.info("Attempting to send the message to the warehouse.")
+        output_warehouse_message.publish(
+            self._basic_publisher,
+            "psd.tol-lab-share"
+        )
+
+        logger.info("Message processing completed.")
         return True
