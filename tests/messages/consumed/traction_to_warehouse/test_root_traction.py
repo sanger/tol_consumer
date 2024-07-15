6a9b88587f5d1921eda7dac5013f3d77ba366e9b
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-07-15 16:16:49 +0100

    Some refactoring on root file

--- tests/messages/consumed/traction_to_warehouse/test_root_traction.py
+++ tests/messages/consumed/traction_to_warehouse/test_root_traction.py
@@ -32,13 +32,17 @@ def subject():
 
 class TestRoot:
     def test_init_passes_arguments_to_record(self):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Record.__init__") as super_init:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Record.__init__"
+        ) as super_init:
             Root(VALID_PAYLOAD)
 
         super_init.assert_called_once_with(VALID_PAYLOAD)
 
     def test_message_uuid_returns_correct_message_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.message_uuid
 
         make_field.assert_called_once_with(MESSAGE_UUID, any(Callable))
@@ -48,7 +52,9 @@ class TestRoot:
         assert subject.message_uuid.value == VALID_PAYLOAD[MESSAGE_UUID].decode()
 
     def test_message_create_date_utc_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.create_date_utc
 
         make_field.assert_called_once_with(CREATED_DATE_UTC)
@@ -58,7 +64,9 @@ class TestRoot:
         assert subject.create_date_utc.value == VALID_PAYLOAD[CREATED_DATE_UTC]
 
     def test_lims_id_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.lims_id
 
         make_field.assert_called_once_with(LIMS_ID)
@@ -68,7 +76,9 @@ class TestRoot:
         assert subject.lims_id.value == VALID_PAYLOAD[LIMS_ID]
 
     def test_lims_uuid_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.lims_uuid
 
         make_field.assert_called_once_with(LIMS_UUID)
@@ -78,7 +88,9 @@ class TestRoot:
         assert subject.lims_uuid.value == VALID_PAYLOAD[LIMS_UUID]
 
     def test_aliquot_type_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.aliquot_type
 
         make_field.assert_called_once_with(ALIQUOT_TYPE)
@@ -88,7 +100,9 @@ class TestRoot:
         assert subject.aliquot_type.value == VALID_PAYLOAD[ALIQUOT_TYPE]
 
     def test_source_type_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.source_type
 
         make_field.assert_called_once_with(SOURCE_TYPE)
@@ -98,7 +112,9 @@ class TestRoot:
         assert subject.source_type.value == VALID_PAYLOAD[SOURCE_TYPE]
 
     def test_source_barcode_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.source_barcode
 
         make_field.assert_called_once_with(SOURCE_BARCODE)
@@ -108,7 +124,9 @@ class TestRoot:
         assert subject.source_barcode.value == VALID_PAYLOAD[SOURCE_BARCODE]
 
     def test_sample_name_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.sample_name
 
         make_field.assert_called_once_with(SAMPLE_NAME)
@@ -118,7 +136,9 @@ class TestRoot:
         assert subject.sample_name.value == VALID_PAYLOAD[SAMPLE_NAME]
 
     def test_used_by_type_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.used_by_type
 
         make_field.assert_called_once_with(USED_BY_TYPE)
@@ -128,7 +148,9 @@ class TestRoot:
         assert subject.used_by_type.value == VALID_PAYLOAD[USED_BY_TYPE]
 
     def test_used_by_barcode_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.used_by_barcode
 
         make_field.assert_called_once_with(USED_BY_BARCODE)
@@ -138,7 +160,9 @@ class TestRoot:
         assert subject.used_by_barcode.value == VALID_PAYLOAD[USED_BY_BARCODE]
 
     def test_volume_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.volume
 
         make_field.assert_called_once_with(VOLUME)
@@ -148,7 +172,9 @@ class TestRoot:
         assert subject.volume.value == VALID_PAYLOAD[VOLUME]
 
     def test_concentration_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.concentration
 
         make_field.assert_called_once_with(CONCENTRATION)
@@ -158,7 +184,9 @@ class TestRoot:
         assert subject.concentration.value == VALID_PAYLOAD[CONCENTRATION]
 
     def test_insert_size_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.insert_size
 
         make_field.assert_called_once_with(INSERT_SIZE)
@@ -168,7 +196,9 @@ class TestRoot:
         assert subject.insert_size.value == VALID_PAYLOAD[INSERT_SIZE]
 
     def test_recorded_at_returns_correct_field(self, subject, any):
-        with patch("tol_lab_share.messages.consumed.traction_to_warehouse.root.Root._make_field") as make_field:
+        with patch(
+            "tol_lab_share.messages.consumed.traction_to_warehouse.traction_to_mlwh_aliquot.Root._make_field"
+        ) as make_field:
             field = subject.recorded_at
 
         make_field.assert_called_once_with(RECORDED_AT)
