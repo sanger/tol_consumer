c07d17edea5647cbd18a0104061cc09144cfc9d8
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-07-23 15:45:47 +0100

    Rollback comments

--- tools/demo/publisher.py
+++ tools/demo/publisher.py
@@ -8,7 +8,7 @@ from lab_share_lib.rabbit.basic_publisher import BasicPublisher
 from lab_share_lib.rabbit.schema_registry import SchemaRegistry
 
 from bioscan_pool_xp_messages import build_bioscan_pool_xp_msg
-from create_labware_messages import build_create_labware_96_msg
+from create_labware_messages import build_create_labware_96_msg, build_update_labware_msg, build_create_tube_msg
 
 REDPANDA_URL = os.getenv("REDPANDA_URL", "http://localhost:8081")
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
@@ -80,11 +80,11 @@ if __name__ == "__main__":
     for pos in range(0, 5):
         if args.message_types == "create-update-labware":
             sample_msg = build_create_labware_96_msg(args.unique_id, pos)
-            # update_msg = build_update_labware_msg(sample_msg)
-            # tube_msg = build_create_tube_msg(args.unique_id, pos)
+            update_msg = build_update_labware_msg(sample_msg)
+            tube_msg = build_create_tube_msg(args.unique_id, pos)
             send_message(sample_msg, "create-labware", encoder, registry, publisher)
-            # send_message(update_msg, "update-labware", encoder, registry, publisher)
-            # send_message(tube_msg, "create-labware", encoder, registry, publisher)
+            send_message(update_msg, "update-labware", encoder, registry, publisher)
+            send_message(tube_msg, "create-labware", encoder, registry, publisher)
         elif args.message_types == "bioscan-pool-xp-to-traction":
             pool_xp_msg = build_bioscan_pool_xp_msg(args.unique_id, pos)
             send_message(pool_xp_msg, "bioscan-pool-xp-tube-to-traction", encoder, registry, publisher)
